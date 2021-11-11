9e5e527e06a026e70b159af15467d5f7ff6fb52e
Rafael Saboia <r-leafar@hotmail.com>
2021-11-11 05:27:09 -0300

    login realizando busca pelo banco

--- pi_turma4/naturalweb.py
+++ pi_turma4/naturalweb.py
@@ -14,6 +14,22 @@ bp = Blueprint('naturalweb', __name__, url_prefix='/naturalweb')
 def index():
     return render_template("index.html");
 
+@bp.route('/teste')
+def teste():
+    cur = get_db().execute("select * from usuario")
+    rs = cur.fetchall()
+    cur.close()
+
+    from werkzeug.security import generate_password_hash, check_password_hash
+  
+    print (rs[0]["nome"])
+    print (check_password_hash(rs[0][2],'1234'))
+
+    #print(generate_password_hash('123',method='pbkdf2:sha256'));
+
+    return "dxdsd"
+
+
 @bp.route('/pontoturistico')
 def pontoturistico():
     return render_template("ponto_turistico.html");
@@ -28,19 +44,47 @@ def imagempontohistorico():
 
 @bp.route('/login',methods = ['POST', 'GET'])
 def login():
+     from werkzeug.security import generate_password_hash, check_password_hash
+    
      if request.method == 'GET':
         return render_template("login.html");
      else:
-         if request.form['user'] == 'admin' and request.form['password']=='123':
+
+        cur = get_db().execute("SELECT senha FROM usuario WHERE nome LIKE ?",[request.form['user']])
+        rs = cur.fetchone()
+    
+        if check_password_hash(rs[0], request.form['password']):
+                     
                      session["user"] = request.form['user']
                      return render_template("index.html");
-         else:
-             flash('Você digitou o usuário e/ou senha inválido')
-             return redirect(url_for("naturalweb.login"));
+        else:
+            flash('Você digitou o usuário e/ou senha inválido')
+            return redirect(url_for("naturalweb.login"));
 
 @bp.route('/cadastrousuario')
 def cadastrousuario():
-    return render_template("cadastro_usuario.html");
+    cur = get_db().execute("select * from usuario")
+    rs = cur.fetchall()
+    cur.close()
+
+    return render_template("cadastro_usuario.html",usuarios=rs);
+
+@bp.route('/editarusuario/<int:idusuario>',methods = ['GET','POST'])
+def editarusuario(idusuario):
+    if request.method == 'GET':
+        cur = get_db().execute("select * from usuario where idusuario=%s"%idusuario)
+        usuario = cur.fetchone()
+        cur.close()
+        return  render_template("edita_usuario.html",usuario=usuario);
+    else:
+        from werkzeug.security import generate_password_hash
+        if request.form['password']==request.form['password_confirm']:
+            cur = get_db().execute("UPDATE usuario set senha=? WHERE idusuario=?",[generate_password_hash(request.form['password']),idusuario])
+            cur.close()
+            get_db().commit()
+            return "atualizado confere"
+        else:
+            return "Não confere"
 
 @bp.route('/cadastroponto')
 def cadastroponto():
--- pi_turma4/schema.sql
+++ pi_turma4/schema.sql
@@ -1,7 +1,17 @@
 DROP TABLE IF EXISTS ponto;
 
 CREATE TABLE ponto (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  idponto INTEGER PRIMARY KEY AUTOINCREMENT,
   titulo TEXT NOT NULL,
   descricao TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS usuario;
+
+CREATE TABLE usuario (
+  idusuario INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT NOT NULL UNIQUE,
+  senha TEXT NOT NULL
+);
+
+insert into usuario (nome,senha) values('admin','pbkdf2:sha256:260000$oQ3eSha76K1m8q0i$b28b40156b2bd5aa154f5562f766b754dc5aaf859b8167eb313fa8ebc297889e');
