13489e3269f351d709b1ede4ae0a48310df5b113
Rafael Saboia <r-leafar@hotmail.com>
2021-11-13 00:13:02 -0300

    Atualizacao

--- pi_turma4/naturalweb.py
+++ pi_turma4/naturalweb.py
@@ -1,14 +1,24 @@
-import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from flask.sessions import NullSession
 from werkzeug.security import check_password_hash, generate_password_hash
-
+import os
+import uuid
+from minio import Minio
+from minio.error import S3Error
 from pi_turma4.db import get_db
 
 bp = Blueprint('naturalweb', __name__, url_prefix='/naturalweb')
+
+
+def client_minio():
+    return  Minio(
+        "031c-191-7-151-185.ngrok.io",
+        access_key="univesp",
+        secret_key="j2mhw82dyu1kn5g4",
+        secure=False
+    )
 
 @bp.route('/')
 def index():
@@ -62,9 +72,9 @@ def cadastrousuario():
         if request.form['password']==request.form['password_confirm']:
             from werkzeug.security import generate_password_hash
 
-            cur = get_db().execute("INSERT INTO usuario (nome,senha) values('%s','%s')"%(request.form['user'],generate_password_hash(request.form['password'])))
+            cur = get_db().execute("INSERT INTO usuario (nome,senha,usuariocadastro) values(?,?,?)",[request.form['user'],generate_password_hash(request.form['password']),session["user"]])
             get_db().commit()
-            
+
             rs = get_lista_usuario()
             flash('Usuario cadastrado com sucesso.')
 
@@ -77,14 +87,16 @@ def cadastrousuario():
 @bp.route('/editarusuario/<int:idusuario>',methods = ['GET','POST'])
 def editarusuario(idusuario):
     if request.method == 'GET':
-        cur = get_db().execute("select * from usuario where idusuario=%s"%idusuario)
+        cur = get_db().execute("select * from usuario where idusuario=?",[idusuario])
         usuario = cur.fetchone()
         cur.close()
         return  render_template("edita_usuario.html",usuario=usuario);
     else:
         from werkzeug.security import generate_password_hash
+        from datetime import datetime, timezone
+
         if request.form['password']==request.form['password_confirm']:
-            cur = get_db().execute("UPDATE usuario set senha=? WHERE idusuario=?",[generate_password_hash(request.form['password']),idusuario])
+            cur = get_db().execute("UPDATE usuario set senha=?,usuarioalteracao=?,alteradoem=? WHERE idusuario=?",[generate_password_hash(request.form['password']),session["user"],datetime.now().strftime("%Y-%m-%d %H:%M:%S"),idusuario])
             cur.close()
             get_db().commit()
             flash('Dados do usuário alterado com sucesso.')
@@ -92,9 +104,39 @@ def editarusuario(idusuario):
         else:
             return "Não confere"
 
-@bp.route('/cadastroponto')
+@bp.route('/cadastroponto',methods = ['GET','POST'])
 def cadastroponto():
-    return render_template("cadastro_ponto.html");
+    
+    if request.method == 'GET':
+        return render_template("cadastro_ponto.html");
+    else:
+        if request.files["nomeimg"]:
+            uploaded_file=request.files["nomeimg"]
+            client = client_minio()
+            size = os.fstat(uploaded_file.fileno()).st_size
+            
+            try:
+                imgname = str(uuid.uuid4())
+                imgname = imgname+"."+uploaded_file.filename.split(".")[1]
+                client.put_object("univesp",imgname,uploaded_file,size)
+                cur = get_db().execute("INSERT INTO ponto (titulo,descricao,nomeimg,usuariocadastro) values(?,?,?,?)",[request.form['nomeponto'],request.form['descricao'],imgname,session["user"]])
+                get_db().commit()
+
+            except S3Error as exc:
+                 print("error occurred.", exc)
+
+           
+
+        '''
+        rs = request.files["nomeimg"].mimetype+"</br></br>"
+        rs += str(os.fstat(request.files["nomeimg"].fileno()).st_size)+"</br></br>"
+        rs += str(request.files["nomeimg"].mimetype_params)+"</br></br>"
+        rs += str(request.files["nomeimg"].content_type)+"</br></br>"
+        rs +=request.form['nomeponto']+"</br></br>"
+        rs +=request.form['tipo']+"</br></br>"
+        rs +=request.form['descricao']+"</br></br>"'''
+
+        return "dsds"
 
 @bp.route('/logout')
 def logout():
--- pi_turma4/schema.sql
+++ pi_turma4/schema.sql
@@ -3,7 +3,12 @@ DROP TABLE IF EXISTS ponto;
 CREATE TABLE ponto (
   idponto INTEGER PRIMARY KEY AUTOINCREMENT,
   titulo TEXT NOT NULL,
-  descricao TEXT NOT NULL
+  descricao TEXT NOT NULL,
+  nomeimg TEXT NOT NULL,
+  criadoem DEFAULT (datetime('now','localtime')),
+  alteradoem DEFAULT (datetime('now','localtime')),
+  usuariocadastro TEXT NOT NULL,
+  usuarioalteracao TEXT
 );
 
 DROP TABLE IF EXISTS usuario;
@@ -11,7 +16,11 @@ DROP TABLE IF EXISTS usuario;
 CREATE TABLE usuario (
   idusuario INTEGER PRIMARY KEY AUTOINCREMENT,
   nome TEXT NOT NULL UNIQUE,
-  senha TEXT NOT NULL
+  senha TEXT NOT NULL,
+  criadoem DEFAULT (datetime('now','localtime')),
+  alteradoem DEFAULT (datetime('now','localtime')),
+  usuariocadastro TEXT NOT NULL,
+  usuarioalteracao TEXT
 );
 
-insert into usuario (nome,senha) values('admin','pbkdf2:sha256:260000$oQ3eSha76K1m8q0i$b28b40156b2bd5aa154f5562f766b754dc5aaf859b8167eb313fa8ebc297889e');
+insert into usuario (nome,senha,usuariocadastro) values('admin','pbkdf2:sha256:260000$oQ3eSha76K1m8q0i$b28b40156b2bd5aa154f5562f766b754dc5aaf859b8167eb313fa8ebc297889e','SISTEMA');
